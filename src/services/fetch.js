24da1e5b2387a03e9026f6feb4ea258cddc37bd6
LunaRocio <106273345+LunaRocio@users.noreply.github.com>
2022-09-22 18:51:00 +0200

    Update fetch.js

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,5 +1,5 @@
 function getDataApi() {
-  return fetch("http://hp-api.herokuapp.com/api/characters")
+  return fetch("https://hp-api.herokuapp.com/api/characters")
     .then((response) => response.json())
     .then((dataApi) => {
       const dataClean = dataApi.map((character, index) => {
